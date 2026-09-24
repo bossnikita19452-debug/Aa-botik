9e8b2c4b7f51457c31956304422347b9d369ca8f
bossnikita19452-debug <boss.nikita19452@gmail.com>
2026-09-24 14:40:17 +0300

    Add strategies/__init__.py

---
+++ strategies/__init__.py
@@ -0,0 +1,7 @@
+# Golden Scanner strategies package
+from strategies.brk_long import scan_brk_long
+from strategies.brk_short import scan_brk_short
+from strategies.mr_long import scan_mr_long
+from strategies.mr_short import scan_mr_short
+
+__all__ = ["scan_brk_long", "scan_brk_short", "scan_mr_long", "scan_mr_short"]
